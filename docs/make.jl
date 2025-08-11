c02d8d432ff4125a65f962b5c0cecb1122ce130a
rowancallahan <rowan.l.callahan@gmail.com>
2025-08-10 20:58:14 -0700

    doc change

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,4 @@ using Documenter, Upset
 
 makedocs(sitename="Upset.jl Documentation", build="build")
 
-deploydocs(repo="https://github.com/rowancallahan/Upset.jl.git",
-	   target="build",
-	   branch="gh-pages"
-	   )
+
